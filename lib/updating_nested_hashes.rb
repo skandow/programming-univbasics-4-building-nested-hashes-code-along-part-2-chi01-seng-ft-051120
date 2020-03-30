74959f8c3cbcd96ba139dca403685c63b69c0c52
Steven Kandow <kando1sr@gmail.com>
2020-03-30 17:21:19 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -60,7 +60,9 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
-update_status(hash, character)
+update_status(character_status)
+  character_status = "dead"
+end 
 
 
 
